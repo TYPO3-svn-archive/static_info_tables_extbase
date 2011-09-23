a3eb3494bd4bf5bd57842df3fff0dc65ad1cbc2a
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-23 11:48:53 +0000

    Version 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_extbase/trunk@52420 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "static_info_tables_extbase".
 #
-# Auto generated 21-09-2011 14:48
+# Auto generated 23-09-2011 13:48
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Provides models and repositories to use the static info tables in Extbase and Fluid.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'dependencies' => 'extbase,fluid,static_info_tables',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"b835";s:17:"ext_localconf.php";s:4:"a096";s:14:"ext_tables.php";s:4:"a096";s:14:"ext_tables.sql";s:4:"d41d";s:24:"ext_typoscript_setup.txt";s:4:"3dcf";s:38:"Classes/Domain/Model/StaticCountry.php";s:4:"3ad2";s:42:"Classes/Domain/Model/StaticCountryZone.php";s:4:"b96f";s:39:"Classes/Domain/Model/StaticCurrency.php";s:4:"b034";s:39:"Classes/Domain/Model/StaticLanguage.php";s:4:"6f1b";s:40:"Classes/Domain/Model/StaticTerritory.php";s:4:"8d4a";s:59:"Classes/Domain/Repository/AbstractStaticEntryRepository.php";s:4:"04ad";s:53:"Classes/Domain/Repository/StaticCountryRepository.php";s:4:"6799";s:57:"Classes/Domain/Repository/StaticCountryZoneRepository.php";s:4:"07ee";s:54:"Classes/Domain/Repository/StaticCurrencyRepository.php";s:4:"ef5c";s:54:"Classes/Domain/Repository/StaticLanguageRepository.php";s:4:"e8b7";s:55:"Classes/Domain/Repository/StaticTerritoryRepository.php";s:4:"f536";s:14:"doc/manual.sxw";s:4:"e810";}',
+	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"b835";s:17:"ext_localconf.php";s:4:"a096";s:14:"ext_tables.php";s:4:"a096";s:14:"ext_tables.sql";s:4:"d41d";s:24:"ext_typoscript_setup.txt";s:4:"3dcf";s:38:"Classes/Domain/Model/StaticCountry.php";s:4:"3ad2";s:42:"Classes/Domain/Model/StaticCountryZone.php";s:4:"b96f";s:39:"Classes/Domain/Model/StaticCurrency.php";s:4:"b034";s:39:"Classes/Domain/Model/StaticLanguage.php";s:4:"6f1b";s:40:"Classes/Domain/Model/StaticTerritory.php";s:4:"8d4a";s:59:"Classes/Domain/Repository/AbstractStaticEntryRepository.php";s:4:"04ad";s:53:"Classes/Domain/Repository/StaticCountryRepository.php";s:4:"6799";s:57:"Classes/Domain/Repository/StaticCountryZoneRepository.php";s:4:"07ee";s:54:"Classes/Domain/Repository/StaticCurrencyRepository.php";s:4:"ef5c";s:54:"Classes/Domain/Repository/StaticLanguageRepository.php";s:4:"e8b7";s:55:"Classes/Domain/Repository/StaticTerritoryRepository.php";s:4:"f536";s:14:"doc/manual.sxw";s:4:"7418";}',
 	'suggests' => array(
 	),
 );
