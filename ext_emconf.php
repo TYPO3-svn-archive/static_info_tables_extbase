3ad4c76b3f12abf304a09b443a658e963e87cc7e
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-12 13:11:19 +0000

    Version 1.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_extbase/trunk@56232 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "static_info_tables_extbase".
 #
-# Auto generated 23-09-2011 13:48
+# Auto generated 12-01-2012 14:10
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,10 +12,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Static Info Tables for Extbase',
-	'description' => 'Provides models and repositories to use the static info tables in Extbase and Fluid.',
+	'description' => 'Provides models and repositories to use the static info tables in extbase and fluid.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'dependencies' => 'extbase,fluid,static_info_tables',
 	'conflicts' => '',
 	'priority' => '',
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 1,
+	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Armin Ruediger Vieweg',
 	'author_email' => 'info@professorweb.de',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"b835";s:17:"ext_localconf.php";s:4:"a096";s:14:"ext_tables.php";s:4:"a096";s:14:"ext_tables.sql";s:4:"d41d";s:24:"ext_typoscript_setup.txt";s:4:"3dcf";s:38:"Classes/Domain/Model/StaticCountry.php";s:4:"3ad2";s:42:"Classes/Domain/Model/StaticCountryZone.php";s:4:"b96f";s:39:"Classes/Domain/Model/StaticCurrency.php";s:4:"b034";s:39:"Classes/Domain/Model/StaticLanguage.php";s:4:"6f1b";s:40:"Classes/Domain/Model/StaticTerritory.php";s:4:"8d4a";s:59:"Classes/Domain/Repository/AbstractStaticEntryRepository.php";s:4:"04ad";s:53:"Classes/Domain/Repository/StaticCountryRepository.php";s:4:"6799";s:57:"Classes/Domain/Repository/StaticCountryZoneRepository.php";s:4:"07ee";s:54:"Classes/Domain/Repository/StaticCurrencyRepository.php";s:4:"ef5c";s:54:"Classes/Domain/Repository/StaticLanguageRepository.php";s:4:"e8b7";s:55:"Classes/Domain/Repository/StaticTerritoryRepository.php";s:4:"f536";s:14:"doc/manual.sxw";s:4:"7418";}',
+	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"b835";s:17:"ext_localconf.php";s:4:"a096";s:14:"ext_tables.php";s:4:"a096";s:14:"ext_tables.sql";s:4:"d41d";s:24:"ext_typoscript_setup.txt";s:4:"5db8";s:38:"Classes/Domain/Model/StaticCountry.php";s:4:"3ad2";s:42:"Classes/Domain/Model/StaticCountryZone.php";s:4:"b96f";s:39:"Classes/Domain/Model/StaticCurrency.php";s:4:"b034";s:39:"Classes/Domain/Model/StaticLanguage.php";s:4:"6f1b";s:40:"Classes/Domain/Model/StaticTerritory.php";s:4:"8d4a";s:59:"Classes/Domain/Repository/AbstractStaticEntryRepository.php";s:4:"04ad";s:53:"Classes/Domain/Repository/StaticCountryRepository.php";s:4:"6799";s:57:"Classes/Domain/Repository/StaticCountryZoneRepository.php";s:4:"07ee";s:54:"Classes/Domain/Repository/StaticCurrencyRepository.php";s:4:"ef5c";s:54:"Classes/Domain/Repository/StaticLanguageRepository.php";s:4:"e8b7";s:55:"Classes/Domain/Repository/StaticTerritoryRepository.php";s:4:"f536";s:14:"doc/manual.sxw";s:4:"7418";}',
 	'suggests' => array(
 	),
 );
